5a029bd4e5e6914e79e6cb44598320086a138f82
momineee <p.rainmakerm@gmail.com>
2022-10-05 10:38:58 +0900

    Calculator
    main 수정

--- src/lion/Day1005/CalculatorMain.java
+++ src/lion/Day1005/CalculatorMain.java
@@ -1,9 +1,9 @@
 public class CalculatorMain {
     public static void main(String[] args) {
-        Calculator userDao = new Calculator();
-        userDao.plus(10, 20);
-        userDao.minus(50, 45);
-        userDao.multiple(6, 7);
-        userDao.divide(10, 20);
+        Calculator calculator = new Calculator();
+        calculator.plus(10, 20);
+        calculator.minus(50, 45);
+        calculator.multiple(6, 7);
+        calculator.divide(10, 20);
     }
 }
