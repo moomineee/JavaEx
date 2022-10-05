a502209316e29b9f501f078329c74a4e193c2963
momineee <p.rainmakerm@gmail.com>
2022-10-05 11:06:52 +0900

    Calculator
    생성자 추가하여 간소화

--- src/lion/Day1005/Calculator.java
+++ src/lion/Day1005/Calculator.java
@@ -1,33 +1,40 @@
-/*Calculator클래스를 만들고 plus minus multiple divide 4개의 메소드를 만들어보세요
-각 메소드 기능은 + - * /이고
-파라메터는 int a, int b 를 받습니다.
-CalculatorMain  클래스에서 실행 해보세요.*/
+//Calculator클래스를 만들고 plus minus multiple divide 4개의 메소드를 만들어보세요
+//각 메소드 기능은 + - * /이고
+//파라메터는 int a, int b 를 받습니다.
+//CalculatorMain  클래스에서 실행 해보세요.
 //DATA ACCESS OBJECT
+
+//생성자 추가하고 매개변수 없애서, 변수를 한번만 할당해서 사칙연산 수행가능하게 코드 수정
+
+
 public class Calculator {
 
-    public void plus(int a, int b){
+    private int a;
+    private int b;
+
+
+    //생성자 추가  =   커맨드 + 인서트
+
+//    public Calculator(int a, int b) {
+
+    public Calculator() { // 생성자를 생성하여 메인메서드 간소화
+        this.a = a;
+        this.b = b;
+    }
+
+
+    public void plus() {
         System.out.println(a + b);
     }
 
-    public void minus(int a, int b){
+    public void minus() {
         System.out.println(a - b);
     }
-    public void multiple(int a, int b){
+    public void multiple() {
         System.out.println(a * b);
     }
-    public void divide(int a, int b){
+    public void divide() {
         float result = (float)a / b;
         System.out.println(result);
     }
 }
-
-
-//public class CalculatorMain {
-//    public static void main(String[] args) {
-//        Calculator calculator = new Calculator();
-//        calculator.plus(3, 3);
-//        calculator.minus(23, 3);
-//        calculator.multiple(34, 3);
-//        calculator.divide(35, 36);
-//    }
-//}
--- src/lion/Day1005/CalculatorMain.java
+++ src/lion/Day1005/CalculatorMain.java
@@ -1,9 +1,15 @@
 public class CalculatorMain {
     public static void main(String[] args) {
-        Calculator calculator = new Calculator();
-        calculator.plus(10, 20);
-        calculator.minus(50, 45);
-        calculator.multiple(6, 7);
-        calculator.divide(10, 20);
+        Calculator calculator = new Calculator(20, 40);
+        calculator.plus();
+        calculator.minus();
+        calculator.multiple();
+        calculator.divide();
+
+//        Calculator calculator = new Calculator();
+//        calculator.plus(10, 20);
+//        calculator.minus(50, 45);
+//        calculator.multiple(6, 7);
+//        calculator.divide(10, 20);
     }
 }
