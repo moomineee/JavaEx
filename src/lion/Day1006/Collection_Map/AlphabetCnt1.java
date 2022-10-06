d639f01b49ae4c895c12f1751daaca56d510769c
momineee <p.rainmakerm@gmail.com>
2022-10-07 00:42:57 +0900

    Collection_Map 아스키코드를 이용해 대,소문자 필터링 후 출력

--- src/lion/Day1006/Collection_Map/AlphabetCnt1.java
+++ src/lion/Day1006/Collection_Map/AlphabetCnt1.java
@@ -30,7 +30,18 @@ public class AlphabetCnt1 {
         }
 
         for (int i = 0; i < s1.length(); i++) {
-            char c = s1.charAt(i);
+//            char c = s1.charAt(i); < 이것만 쓰면 오류가 나는 이유
+            // NullPointerExcep은 말그대로 Null(존재하지 않는값)을 참조할려고 했다는 것인데
+            // 이 뜻은 alphabetMap 이라는 Map자료구조에 key값으로 존재하지 않는 것을 참조(.get()) 했다는 것.
+            //
+            //그래서 25번째 줄보면 a~z 뿐만아니라 / : 와 같은 특수기호도 있는데
+            // 28번째 줄 for문을 통해 초기화된 alphabetMap은 A~Z만 key로 가지고 있기에
+            // 32번째 for문에서 s1을 순회하면서 특수기호를 만나는 순간 발생한 에러
+            // 따라서 아래와 같이 추가해줘야함.
+            char c = Character.valueOf('A'); // 'A'와 같은 타입만 참조하겠다.
+            System.out.println(c);
+            System.out.println(c == 'A' );
+
             boolean isAlphabet = alphabetCnt.isAlphabet(c);
             if (isAlphabet) {
                 alphabetMap.put(c, alphabetMap.get(c) + 1);
