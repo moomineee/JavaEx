9d40e71b1c7f13401a98fdc519e64ffcf2163141
momineee <p.rainmakerm@gmail.com>
2022-10-06 10:52:06 +0900

    Collection - Generate Alphabet (랜덤 기능 추가)
    랜덤 기능 추가

--- src/lion/Day1006/Collection_GenerateAlphabet/RandomAlphabet.java
+++ src/lion/Day1006/Collection_GenerateAlphabet/RandomAlphabet.java
@@ -4,15 +4,26 @@ package week.day3.collection2;
 //알파벳 A ~ Z 까지 랜덤한 알파벳을 50개 생성하고 중복제거 해보세요.
 //26 0~25
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class RandomAlphabet {
     public static void main(String[] args) {
         AlphabetGenerator alphabetGenerator = new AlphabetGenerator();
         char alphabet = alphabetGenerator.generate(0);
         System.out.println(alphabet);
 
-        char alphabetA = 'A';
-        char alphabet1 = (char)(alphabetA + 10);
-        System.out.println(alphabet1);
+//        char alphabetA = 'A'; // 출력하는 다른 방법
+//        char alphabet1 = (char)(alphabetA + 10);
+//        System.out.println(alphabet1);
 
+        Set<Character> characters = new HashSet<>();
+        int cnt = 0;
+        while (cnt < 100) {
+            characters.add(alphabetGenerator.generate(0));
+            cnt += 1;
+        }
+        System.out.println(characters);
+        System.out.println(characters.size());
     }
 }
