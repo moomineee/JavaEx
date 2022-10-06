37c1fa174e102a7e2b44bbee36762c60a8430938
momineee <p.rainmakerm@gmail.com>
2022-10-06 15:13:11 +0900

    File 찾기 - 현재 디렉토리 첫번째 파일 & 파일 리스트 전체 출력

---
+++ src/lion/Day1006/File_Data/FileExercise.java
@@ -0,0 +1,22 @@
+package week.day3.fileread;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+
+public class FileExercise {
+    public static void main(String[] args) throws FileNotFoundException {
+        // ./ (현재의 디렉토리를 지정하는 것)
+        //file을 읽어보자. 근데 나는 여기에 없음
+//        FileReader fileReader = new FileReader("./a_file");
+
+        //파일의 경로를 찾아보자. 디렉토리로 접근하는 방법
+        File dir = new File("./");
+        File files[] = dir.listFiles();
+        System.out.println(files[0]); // 현재 디렉토리의 첫번째 파일 읽기  ./Java1006.iml
+
+        for(File file : files) {
+            System.out.println(file); // 현재 디렉토리에 있는 파일들을 전부 출력
+        }
+    }
+}
