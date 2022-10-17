ffab7da687fca598ea73d39b471aa26651e369a8
momineee <p.rainmakerm@gmail.com>
2022-10-17 09:26:56 +0900

    별찍기 - RightTriangle

--- src/lion/Week5/Day10.17/RightTriangle.java
+++ src/lion/Week5/Day10.17/RightTriangle.java
@@ -1,21 +1,25 @@
-package com.week5;
+package com.week5.day1;
 
 // 별찍기
+
+import java.util.Scanner;
 
 public class RightTriangle {
     public static void main(String[] args) {
 
-        System.out.println("*");
-        System.out.println("**");
-        System.out.println("***");
-        System.out.println("****");
+//        System.out.println("*");
+//        System.out.println("**");
+//        System.out.println("***");
+//        System.out.println("****");
 
+        Scanner scanner = new Scanner(System.in);
+//        int n = scanner.nextInt();
 
         for (int i = 0; i < 4; i++) {
-            for (int j = 4 ; j > i ; j--) {
+            for (int j = 1 ; j <= i + 1 ; j++) {
                 System.out.print("*");
             }
-            System.out.println();
+            System.out.printf("%n");
         }
 
     }
