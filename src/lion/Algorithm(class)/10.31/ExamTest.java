fdf8dcd510f0bd541b5804b1bc098b748f124b1f
momineee <p.rainmakerm@gmail.com>
2022-10-31 09:41:58 +0900

    완전탐색 - 모의고사(프로그래머스)

---
+++ src/lion/Algorithm(class)/10.31/ExamTest.java
@@ -0,0 +1,20 @@
+package javalion.week7;
+
+public class ExamTest {
+    public int[]  solution(int[] answers) {
+        int [] answer = {};
+        int [] person1 = {1,2,3,4,5};
+        int [] person2 = {2,1,2,3,2,4,2,5};
+        int [] person3 = {3,3,1,1,2,2,4,4,5,5};
+
+        int answer1 = 0;
+        int answer2 = 0;
+        int answer3 = 0;
+
+        for (int i = 0; i < answer.length; i++) {
+            if(person1[i%
+
+        }
+        return answer;
+    }
+}
