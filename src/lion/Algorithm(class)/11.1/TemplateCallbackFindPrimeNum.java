d21963fd9332f54e35d5a91e7bd84713be3a6f3b
momineee <p.rainmakerm@gmail.com>
2022-11-01 10:31:52 +0900

    소수찾기 - Templatecallback pattern - interface 적용

--- src/lion/Algorithm(class)/11.1/TemplateCallbackFindPrimeNum.java
+++ src/lion/Algorithm(class)/11.1/TemplateCallbackFindPrimeNum.java
@@ -1,16 +1,17 @@
 package javalion.week7.day2;
 
+// 인터페이스 적용
+interface StatementStrategy {
+    boolean compare (int a, int b); // 2개를 받아서 boolean을 return
+}
+
 public class TemplateCallbackFindPrimeNum {
 // i < num
 // i < num / 2
 // i * i <= num  (=  Math.sqrt(num) 그런데 이 math.sqrt가 연산이 많으므로 i*i를 쓴다.)
-
-    boolean someOperation(int a, int b) {
-        return a < b;
-    }
-
-    boolean isPrime(int num) {
-        for (int i = 2; someOperation(i, num); i++) {
+    
+    boolean isPrime(int num, StatementStrategy stmt) {
+        for (int i = 2; stmt.compare(i, num); i++) {
             if(num % i == 0) {
                 return false;
             }
