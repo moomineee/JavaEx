03c0391d2521f1d8284c467b46ea7743951b9f38
momineee <p.rainmakerm@gmail.com>
2022-11-01 10:59:59 +0900

    소수찾기 - TemplateCallback pattern 적용 (최종)

--- src/lion/Algorithm(class)/11.1/TemplateCallbackFindPrimeNum.java
+++ src/lion/Algorithm(class)/11.1/TemplateCallbackFindPrimeNum.java
@@ -1,20 +1,16 @@
 package javalion.week7.day2;
 
-import java.util.Comparator;
-
 // 인터페이스 적용
 interface StatementStrategy {
     boolean compare (int a, int b); // 2개를 받아서 boolean을 return
 }
-
 public class TemplateCallbackFindPrimeNum {
-// i < num
-// i < num / 2
-// i * i <= num  (=  Math.sqrt(num) 그런데 이 math.sqrt가 연산이 많으므로 i*i를 쓴다.)
-
-    boolean isPrime(int num, StatementStrategy stmt) {
-//        Comparator<Integer> comparator = (a, b) -> a + b;
-        for (int i = 2; stmt.compare(i, num); i++) {
+// 3개의 소수찾기 방법
+// (1). i < num
+// (2). i < num / 2
+// (3). i * i <= num  (=  Math.sqrt(num) 그런데 이 math.sqrt가 연산이 많으므로 i*i를 쓴다.)
+    boolean isPrime(int num, StatementStrategy stmt) { // interface를 parameter로 받는다.
+        for (int i = 2; stmt.compare(i, num); i++) { //
             if(num % i == 0) {
                 return false;
             }
@@ -22,15 +18,10 @@ public class TemplateCallbackFindPrimeNum {
         return true;
     }
 
-
     public static void main(String[] args) {
         TemplateCallbackFindPrimeNum tcfp = new TemplateCallbackFindPrimeNum();
-        boolean r = tcfp.isPrime(17, new StatementStrategy() {
-            @Override
-            public boolean compare(int a, int b) {
-                return a * a < b; //  b의 제곱근보다 작을때까지 실행
-            }
-        });
-        System.out.println(r);
+        System.out.println(tcfp.isPrime(13, (a, b) -> a < b));  // 람다 왜 쓰나? 간편하니까.
+        System.out.println(tcfp.isPrime(17, (a, b) -> a < b / 2));
+        System.out.println(tcfp.isPrime(19, (a, b) -> a * a < b));
     }
 }
