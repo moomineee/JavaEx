0df4bc16b1118b869083731824d522c988d437a1
momineee <p.rainmakerm@gmail.com>
2022-11-03 09:28:11 +0900

    에라토스테네스의 체 - 알고리즘 분석

---
+++ src/lion/Algorithm(class)/11.3/RemoveMultipleOf.java
@@ -0,0 +1,17 @@
+package javalion.week7.day4;
+
+public class RemoveMultipleOf {
+// 2부터 50까지 숫자가 들어있는 Array 만들기
+// 만든 Array크기의 checks배열을 만든다.
+// 만든 Array대신 checks배열에 2를 제외한 모든 2의 배수에 false로 바꾸는 로직
+// 소수만 출력 해보기: checks에 true인 nums[i]만 출력
+
+// 아래를 출력할 수 있으면 80%는 풀어낸 것
+// 2, 2
+// 3, 4
+// 4, 6
+// 5, 8
+
+    public static void main(String[] args) {
+    }
+}
