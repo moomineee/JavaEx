93c167a373b860f4cc72eb01b904498fa9365a5f
momineee <p.rainmakerm@gmail.com>
2022-11-09 09:17:11 +0900

    selectionSort

---
+++ src/lion/Algorithm(class)/11.9/SelectionSort.java
@@ -0,0 +1,15 @@
+package javalion.week8.day3;
+
+public class SelectionSort {
+
+    public static void main(String[] args) {
+
+        int [] arr = new int[] {2, 7, 4, 9, 10, 223, 111, 23, 3, 39};
+
+        int i = 0;
+        int minIdx = 0;
+        // i = 0일 때 arr[0]에 들어갈 최소값이 들어있는 idx 찾기
+
+
+    }
+}
