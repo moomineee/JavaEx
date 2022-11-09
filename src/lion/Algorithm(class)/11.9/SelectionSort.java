0ca40fc4ff61e787af3a86018b96d8456fcca5ff
momineee <p.rainmakerm@gmail.com>
2022-11-09 09:29:19 +0900

    SelectionSort 구현

--- src/lion/Algorithm(class)/11.9/SelectionSort.java
+++ src/lion/Algorithm(class)/11.9/SelectionSort.java
@@ -1,5 +1,7 @@
 package javalion.week8.day3;
 
+import java.util.Arrays;
+
 public class SelectionSort {
 
     public static void main(String[] args) {
@@ -8,8 +10,20 @@ public class SelectionSort {
 
         int i = 0;
         int minIdx = 0;
+        int temp = 0;
         // i = 0일 때 arr[0]에 들어갈 최소값이 들어있는 idx 찾기
 
-
+        for (i = 0; i < arr.length - 1; i++) {
+            minIdx = i;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] < arr[minIdx]) {
+                    minIdx = j;
+                }
+            }
+        temp = arr[minIdx];
+        arr[minIdx] = arr[i];
+        arr[i] = temp;
+        }
+        System.out.println(Arrays.toString(arr));
     }
 }
