3a16dd5468c52884e2324520e32ba29399ee1ff1
momineee <p.rainmakerm@gmail.com>
2022-11-11 09:17:30 +0900

    [프로그래머스] 비밀지도 - 알고리즘 분석

---
+++ src/lion/Algorithm(class)/11.11/TreasureMap.java
@@ -0,0 +1,12 @@
+package javalion.week8.day5;
+
+// 1. 10진수를 2진수로 변환 -> Integer.toBianryString()
+// 2. 배열 2개 생성
+// 3. 2진수로 변환한 수의 길이가 2^n-1보다 작을 경우(앞부분이 0으로 표시될 경우) 0을 입력한다.
+// 4. 1을 #으로 바꾸고, 0을 공백(" ")으로 바꾼다. -> .replace
+
+public class TreasureMap {
+
+
+
+}
