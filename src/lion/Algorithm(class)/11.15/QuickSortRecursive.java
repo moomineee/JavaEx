5bcc84f2e0e29326fd46e434049b27db39dcdf06
momineee <p.rainmakerm@gmail.com>
2022-11-15 10:35:19 +0900

    QuickSort - 재귀 사용 - 문제점 해결해야함

---
+++ src/lion/Algorithm(class)/11.15/QuickSortRecursive.java
@@ -0,0 +1,53 @@
+package javalion.week9.day2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+//    List<Integer>, List<Integer>, List<Integer>에 담는다.
+public class QuickSortRecursive {
+    // list 합치는 것
+    public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right) {
+        List<Integer> answer = new ArrayList<>();
+        answer.addAll(left);
+        answer.addAll(mid);
+        answer.addAll(right);
+        return answer;
+    }
+
+    public List<Integer> sort(List<Integer> arr) {
+
+        // 1. 기준값 뽑는 로직 구현
+        int pivot = arr.get(arr.size() / 2);
+        System.out.println(pivot);
+
+        // 2. 기준값 기준으로 좌 우로 나누어 담는 로직 구현
+        List<Integer> left = new ArrayList<>();
+        List<Integer> right = new ArrayList<>();
+        List<Integer> mid = new ArrayList<>();
+
+        for (int i = 0; i < arr.size(); i++) {
+            if(pivot > arr.get(i)) {
+                left.add(arr.get(i));
+            } else if (pivot < arr.get(i)) {
+             right.add(arr.get(i));
+            } else {
+                mid.add(arr.get(i));
+            }
+        }
+        System.out.println(left);
+        System.out.println(right); // 20, 18, 19 25, 40, 50
+
+        // list를 합치는 연산
+        return merge(sort(left), sort(mid), sort(right)); // 재귀의 경우 retrun하기 전 작아진 단위를 sort해야 함.
+    }
+
+    public static void main(String[] args) {
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+        List<Integer> al = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            al.add(arr[i]);
+        }
+        QuickSortRecursive qr = new QuickSortRecursive();
+        qr.sort(al);
+    }
+}
