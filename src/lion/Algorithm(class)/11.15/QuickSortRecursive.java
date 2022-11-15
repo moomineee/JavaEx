6c0533d53cb5ee1a853f66b84bf60500c6f3b24f
momineee <p.rainmakerm@gmail.com>
2022-11-15 10:50:52 +0900

    QuickSort - 재귀를 이용한 정렬 - 끝.

--- src/lion/Algorithm(class)/11.15/QuickSortRecursive.java
+++ src/lion/Algorithm(class)/11.15/QuickSortRecursive.java
@@ -3,7 +3,8 @@ package javalion.week9.day2;
 import java.util.ArrayList;
 import java.util.List;
 
-//    List<Integer>, List<Integer>, List<Integer>에 담는다.
+
+// 탈출 조건 추가
 public class QuickSortRecursive {
     // list 합치는 것
     public List<Integer> merge(List<Integer> left, List<Integer> mid, List<Integer> right) {
@@ -16,11 +17,17 @@ public class QuickSortRecursive {
 
     public List<Integer> sort(List<Integer> arr) {
 
+        // 3. 재귀 탈출 조건
+        if (arr.size() <= 1) {
+            return arr;
+        }
+
         // 1. 기준값 뽑는 로직 구현
         int pivot = arr.get(arr.size() / 2);
         System.out.println(pivot);
 
         // 2. 기준값 기준으로 좌 우로 나누어 담는 로직 구현
+        // List<Integer>, List<Integer>, List<Integer>에 담는다.
         List<Integer> left = new ArrayList<>();
         List<Integer> right = new ArrayList<>();
         List<Integer> mid = new ArrayList<>();
@@ -38,7 +45,8 @@ public class QuickSortRecursive {
         System.out.println(right); // 20, 18, 19 25, 40, 50
 
         // list를 합치는 연산
-        return merge(sort(left), sort(mid), sort(right)); // 재귀의 경우 retrun하기 전 작아진 단위를 sort해야 함.
+        return merge(sort(left), mid, sort(right)); // 재귀의 경우 retrun하기 전 작아진 단위를 sort해야 함.
+        // mid에 sort하면 무한루프
     }
 
     public static void main(String[] args) {
