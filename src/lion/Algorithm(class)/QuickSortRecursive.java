f137318a56bec99296e0475151c7c94c188af973
momineee <p.rainmakerm@gmail.com>
2022-11-15 09:19:18 +0900

    QuickSort - 재귀 사용(시작)

---
+++ src/lion/Algorithm(class)/QuickSortRecursive.java
@@ -0,0 +1,13 @@
+package javalion.week9.day2;
+
+// 1. 기준값 뽑는 로직 구현
+// 2. 기준값 기준으로 좌 우로 나누어 담는 로직 구현
+//    List<Integer>, List<Integer>, List<Integer>에 담는다.
+public class QuickSortRecursive {
+    public static void main(String[] args) {
+
+        var arr = new int[]{20, 18, 5, 19, 5, 25, 40, 50};
+
+
+    }
+}
