485abb14abd41a7e2a0273a6e2305949051b780a
momineee <p.rainmakerm@gmail.com>
2022-11-16 10:27:47 +0900

    QuickSort - Array 사용 Pivot까지 leftIdx, rightIdx 증가시키기

--- src/lion/Algorithm(class)/11.16/QuickSortArray.java
+++ src/lion/Algorithm(class)/11.16/QuickSortArray.java
@@ -8,5 +8,26 @@ public class QuickSortArray {
         var arr = new int[] {20, 18, 5, 19, 40, 50, 5, 25};
         int pivot = arr[arr.length / 2 ];
         System.out.println(pivot);
+
+        int leftIdx = 0;
+        int rightIdx = arr.length - 1;
+        while(arr[leftIdx] < pivot) {
+            leftIdx += 1;
+            System.out.println();
+        }
+        while(arr[rightIdx] > pivot) {
+            leftIdx -= 1;
+        }
+
+        int temp = arr[leftIdx];
+        arr[leftIdx] = arr[rightIdx];
+        arr[rightIdx] = temp;
+
+        leftIdx += 1; // 교환했으므로 +1
+        rightIdx -= 1; // 교환했으므로 -1
+
+        System.out.printf("4:%d 7:%d", arr[4], arr[7]);
+        System.out.printf("leftIdx:%d right:%d%n", leftIdx, rightIdx);
     }
 }
+
