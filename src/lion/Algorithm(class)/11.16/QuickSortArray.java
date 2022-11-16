a028fb1c6a70b0bee13f70bfb1c4ad608b4ac692
momineee <p.rainmakerm@gmail.com>
2022-11-16 09:29:28 +0900

    QuickSort - Array 이용 (시작)

---
+++ src/lion/Algorithm(class)/11.16/QuickSortArray.java
@@ -0,0 +1,12 @@
+package javalion.week9.day3;
+
+public class QuickSortArray {
+
+
+
+    public static void main(String[] args) {
+        var arr = new int[] {20, 18, 5, 19, 40, 50, 5, 25};
+        int pivot = arr[arr.length / 2 ];
+        System.out.println(pivot);
+    }
+}
