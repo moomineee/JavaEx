b846a82835db01dd8e0c6a34a1f2051c3b5be4e2
momineee <p.rainmakerm@gmail.com>
2022-11-29 09:17:16 +0900

    문자열 조합 - n개의 문자열을 재귀를 이용하여 출력2 문자를 char형태로 출력하고 prefix를 갱신하는 방식으로 메소드 작성

---
+++ src/lion/Algorithm(class)/11.28/PrintAtoZCombination.java
@@ -0,0 +1,17 @@
+package javalion.week11.day1;
+
+public class PrintAtoZCombination {
+    public static final String chars ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+    public static void printAlphabet(String prefix, int depth) {
+        if (prefix.length() > depth) return;
+        System.out.println(prefix);
+
+        for (int i = 0; i < chars.length(); i++) {
+            printAlphabet(prefix + chars.charAt(i), depth);
+        }
+    }
+
+    public static void main(String[] args) {
+        printAlphabet("", 2);
+    }
+}
